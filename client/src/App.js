088915a7a8550e3e6b98519cceac1a6396cffc59
Faizanshaikh12 <faizanroy000777@gmail.com>
2022-03-24 01:16:27 +0530

    - Implement home page routes and design

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route, } from "react-router-dom";
+import Home from "./pages/Home";
+import EditorPage from "./pages/EditorPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Faizan Shaikh</h1>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/editor/:roomId' element={<EditorPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/EditorPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function EditorPage(props) {
+    return (
+        <div>Edit</div>
+    );
+}
+
+export default EditorPage;
